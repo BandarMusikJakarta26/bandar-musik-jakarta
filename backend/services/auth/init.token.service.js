440e5f2b45f499c8e5a8d5cbe047a63aaba678b9
Cehmenang <heaven.cehwin12@gmail.com>
2024-12-19 14:07:29 +0700

    Tambah setingan cookies

--- backend/services/auth/init.token.service.js
+++ backend/services/auth/init.token.service.js
@@ -7,8 +7,8 @@ export default async function initToken(req, res){
     const accessToken =  jwt.sign({ id: req.id }, process.env.JWT_SECRET, { expiresIn: '10s' })
     const refreshToken =  jwt.sign({ id: req.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
-    res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, maxAge: 10000, sameSite: false })
-    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, maxAge: 1000 * 3600, sameSite: false })
+    res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, maxAge: 10000, sameSite: 'None' })
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, maxAge: 1000 * 3600, sameSite: 'None' })
 
     const akun = await db.akun.update({ where: { id: req.id }, data: { refreshToken } })
     
