9d60b4897be81533b7b490c0fcced4cf15e4ef16
Cehmenang <heaven.cehwin12@gmail.com>
2024-12-20 14:26:20 +0700

    Sudah proper fix

--- backend/services/auth/init.token.service.js
+++ backend/services/auth/init.token.service.js
@@ -7,13 +7,8 @@ export default async function initToken(req, res){
     const accessToken =  jwt.sign({ id: req.id }, process.env.JWT_SECRET, { expiresIn: '10s' })
     const refreshToken =  jwt.sign({ id: req.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
-    // Production Mode
-    res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, maxAge: 10000, sameSite: 'None' })
-    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, maxAge: 1000 * 3600, sameSite: 'None' })
-
-    // Development Mode
-    // res.cookie('accessToken', accessToken, { httpOnly: true, secure: false, maxAge: 10000 })
-    // res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: false, maxAge: 1000 * 3600 })
+    res.cookie('accessToken', accessToken, { httpOnly: true, secure: process.env.ORIGIN ? false : true, maxAge: 10000, sameSite: process.env.ORIGIN ? true : 'None' })
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: process.env.ORIGIN ? false : true, maxAge: 1000 * 3600, sameSite: process.env.ORIGIN ? true : 'None' })
 
     const akun = await db.akun.update({ where: { id: req.id }, data: { refreshToken } })
     
--- frontend/libs/config.ts
+++ frontend/libs/config.ts
@@ -2,6 +2,5 @@ import { Cloudinary } from "@cloudinary/url-gen/index"
 
 const cloudName = 'dhz1p2xvn'
 
-export const host = "https://bandarmusikjakartaserver.vercel.app"
-// export const host = "http://localhost:5000"
+export const host = process.env.NODE_ENV == "production" ? "https://bandarmusikjakartaserver.vercel.app" : "http://localhost:5000"
 export const cloudSDK = new Cloudinary({ cloud: { cloudName: cloudName } })
