c0cb53b74bebab39dc09d7fdb5ca3726cb6b588c
Cehmenang <heaven.cehwin12@gmail.com>
2024-12-23 19:46:18 +0700

    Melengkapi fitur Search kembali

--- frontend/src/components/navbar/TabPencarian.tsx
+++ frontend/src/components/navbar/TabPencarian.tsx
@@ -4,7 +4,7 @@ import SearchProducts from "./SearchProducts";
 
 export default function TabPencarian({ result }: { result: { products: any[], brands: any[], categories: any[] } }){
     return (
-        <div className={`${result.products.length == 0 && result.brands.length == 0 && result.categories.length == 0 ? 'h-0' : false} pencarian absolute bg-primary top-[10px] text-[14px] w-full h-[300px] overflow-y-scroll`}>
+        <div className={`${result.products.length == 0 && result.brands.length == 0 && result.categories.length == 0 ? 'h-0 hidden' : false} pencarian absolute bg-primary top-[10px] text-[14px] w-full h-[300px] overflow-y-scroll`}>
             { result.categories.length > 0 ? <SearchCategories categories={result.categories}/> : null}
             { result.brands.length > 0 ? <SearchBrands brands={result.brands}/> : null}
             { result.products.length > 0 ? <SearchProducts products={result.products}/> : null}
