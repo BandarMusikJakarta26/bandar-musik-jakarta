0bfedb5c16b0bbe2d5a9cf5afc6005c3d6a037a7
Cehmenang <heaven.cehwin12@gmail.com>
2024-12-27 12:23:11 +0700

    Responsive Mobile 4

--- frontend/src/action/screen.action.ts
+++ frontend/src/action/screen.action.ts
@@ -1,5 +1,3 @@
-import { NavigateFunction } from "react-router"
-
 export default function responsivePage(setScreen: React.SetStateAction<number|any>){
         return addEventListener("resize", ()=>{
             setScreen(window.innerWidth)
