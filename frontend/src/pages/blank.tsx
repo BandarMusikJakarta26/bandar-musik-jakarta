9b45ea52d37bb30d4977e6c34ab5d8b4f6f2161b
Cehmenang <heaven.cehwin12@gmail.com>
2024-12-28 11:58:23 +0700

    Tambah responsive brand & authenticate

--- frontend/src/pages/blank.tsx
+++ frontend/src/pages/blank.tsx
@@ -1,7 +1,8 @@
 export default function BlankPage(){
     return (
-        <div className="blank h-[78vh] flex items-center justify-center">
-            <h1 className="text-[120px] font-extrabold tracking-tight">Not Found</h1>
+        <div className="blank h-[78vh] flex flex-col items-center justify-center">
+            <h1 className="not-found text-[40px] md:text-[120px] font-extrabold tracking-tight">Not Found</h1>
+            <p className="text-[14px] -mt-[8px]">kembali ke <a href="/" className="underline">halaman utama</a>?</p>
         </div>
     )
 }
