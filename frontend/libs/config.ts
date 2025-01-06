49ad57c1ae9fcd8fd2c8dd06a34505c0c09e6860
BandarMusikJakarta26 <bandarmusikindonesia@gmail.com>
2025-01-06 14:12:11 +0700

    Perbaiki Buf

--- frontend/libs/config.ts
+++ frontend/libs/config.ts
@@ -1,6 +1,6 @@
 import { Cloudinary } from "@cloudinary/url-gen/index"
 
-const cloudName = 'dhz1p2xvn'
+const cloudName = 'dciaquu2u'
 
 export const host = process.env.NODE_ENV == "production" ? "https://bandarmusikjakartaserver.vercel.app" : "http://localhost:5000"
 export const cloudSDK = new Cloudinary({ cloud: { cloudName: cloudName } })
